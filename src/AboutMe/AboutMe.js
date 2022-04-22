487a5271db0849d1ce2546cf3f2bf0685ac1355a
Franco <phanfranco@yahoo.com>
2022-04-21 17:08:27 -0700

    feat(About Me): content for about me section completed semi-completed

--- src/AboutMe/AboutMe.js
+++ src/AboutMe/AboutMe.js
@@ -1,8 +1,43 @@
 import React from "react";
-import { AboutMeWrapper } from "./AboutMe.styles";
+import photo from "../assets/profile pic.jpg";
+import {
+  AboutMeContainer,
+  AboutMeSection,
+  AboutMeWrapper,
+  Photo,
+  PhotoSection,
+  SectionText,
+  SectionTitle,
+  Skills,
+  SkillSection,
+} from "./AboutMe.styles";
 
 const AboutMe = () => {
-  return <AboutMeWrapper>AboutMe</AboutMeWrapper>;
+  return (
+    <AboutMeWrapper>
+      <AboutMeContainer>
+        <AboutMeSection>
+          <SectionTitle>About Me</SectionTitle>
+          <SectionText>
+            Greetings, My name is Franco Phan and I love building things for the
+            web. My interest in software engineering really shaped when I
+          </SectionText>
+          <SkillSection>
+            <Skills>Javascript (ES6+)</Skills>
+            <Skills>React JS</Skills>
+            <Skills>MongoDB</Skills>
+            <Skills>Node JS</Skills>
+            <Skills>Express JS</Skills>
+            <Skills>CSS</Skills>
+            <Skills>styled-components</Skills>
+          </SkillSection>
+        </AboutMeSection>
+        <PhotoSection>
+          <Photo src={photo} />
+        </PhotoSection>
+      </AboutMeContainer>
+    </AboutMeWrapper>
+  );
 };
 
 export default AboutMe;
--- src/AboutMe/AboutMe.styles.js
+++ src/AboutMe/AboutMe.styles.js
@@ -9,3 +9,46 @@ export const AboutMeWrapper = styled.div`
   width: 100vw;
   background-color: darkgrey;
 `;
+
+export const AboutMeContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  height: 50vh;
+  width: 50vw;
+`;
+
+export const AboutMeSection = styled.div`
+  display: flex;
+  flex-direction: column;
+  // justify-content: center;
+  height: 50vh;
+  width: 50vw;
+`;
+
+export const PhotoSection = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  width: 50%;
+`;
+
+export const Photo = styled.img`
+  object-fit: contain;
+`;
+
+export const SectionTitle = styled.h1`
+  margin: 0;
+`;
+
+export const SectionText = styled.p`
+  margin: 0;
+`;
+
+export const SkillSection = styled.ul`
+  margin: 0;
+`;
+
+export const Skills = styled.li`
+  margin: 0;
+`;
