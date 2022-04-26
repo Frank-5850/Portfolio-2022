1defc4faa115e1a6cb0e31f56ac9ea13fcc33e97
Franco <phanfranco@yahoo.com>
2022-04-26 15:51:23 -0700

    feat(landingPage): landing page now responsive to iphone.

--- src/LandingPage/landingPage.styles.js
+++ src/LandingPage/landingPage.styles.js
@@ -15,6 +15,10 @@ export const LandingPageContainer = styled.div`
   justify-content: center;
   height: 50vh;
   width: 50vw;
+  @media (max-width: 414px) {
+    height: 100vh;
+    width: 100vw;
+  }
 `;
 
 export const Greeting = styled.h3`
@@ -24,6 +28,9 @@ export const Greeting = styled.h3`
 export const HeaderText = styled.h1`
   font-size: 3em;
   margin: 0 50px;
+  @media (max-width: 414px) {
+    font-size: 2em;
+  }
 `;
 
 export const Skills = styled.h4`
